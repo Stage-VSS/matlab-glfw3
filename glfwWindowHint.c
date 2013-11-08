4fa9dbdf5e6d2b48082643135c1a3fa3aa789318
Mark Cafaro <markcafaro@d-128-95-10-211.dhcp4.washington.edu>
2013-11-07 16:12:34 -0800

    Window hints and constants

---
+++ glfwWindowHint.c
@@ -0,0 +1,16 @@
+#include <mex.h>
+#include <GLFW/glfw3.h>
+
+void mexFunction(int nlhs, mxArray *plhs[], int nrhs, const mxArray *prhs[])
+{
+	if (nrhs != 2)
+	{
+		mexErrMsgIdAndTxt("glfw:usage", "Usage: glfwWindowHint(target, hint)");
+		return;
+	}
+	
+	int target = mxGetScalar(prhs[0]);
+	int hint = mxGetScalar(prhs[1]);
+		
+	glfwWindowHint(target, hint);
+}
